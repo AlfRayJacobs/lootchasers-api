dc1d55c004cdfae078f356c0471eb63e075c0708
ausdnn <59588876+ausdnn@users.noreply.github.com>
2025-04-05 11:07:58 -0700

    Update RuneliteController.cs
    missed null operator

--- LootchasersAPI/Controllers/RuneliteController.cs
+++ LootchasersAPI/Controllers/RuneliteController.cs
@@ -35,7 +35,7 @@ public class RuneliteController : ControllerBase
         var payloadJson = form["payload_json"];
         Console.WriteLine($"Received JSON Payload: {payloadJson}");
         
-        if (JsonParser.GetNodeFromJson(payloadJson, "clanName") != "LootChasers")
+        if (JsonParser.GetNodeFromJson(payloadJson!, "clanName") != "LootChasers")
             return BadRequest("Invalid clan");
             
         var file = form.Files["file"];
