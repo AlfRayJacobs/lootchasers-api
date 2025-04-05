4ec2696c53f558f115f91d34959ce33737747c27
alfray <adam.alfray.jacobs@gmail.com>
2025-04-05 23:29:26 +0100

    Apply ruleset logic to incoming requests

---
+++ LootchasersAPI/Classes/RuleSets.cs
@@ -0,0 +1,40 @@
+﻿using LootchasersAPI.Interfaces;
+using System.Text.Json;
+
+namespace LootchasersAPI.Classes.RuleSets
+{
+    public class LootRuleset : IRuleSet
+    {
+        private readonly int MIN_VALUE = 100000;
+
+        public bool ShouldSendNotification(string jsonContent) => GetItemCost(jsonContent) > MIN_VALUE;
+
+        private decimal? GetItemCost(string jsonContent)
+        {
+            try
+            {
+                using (JsonDocument doc = JsonDocument.Parse(jsonContent))
+                {
+                    JsonElement itemsArray = doc.RootElement.GetProperty("extra").GetProperty("items");
+
+                    if (itemsArray.GetArrayLength() > 0)
+                    {
+                        JsonElement item = itemsArray[0];
+
+                        decimal priceEach = item.GetProperty("priceEach").GetDecimal();
+                        int quantity = item.GetProperty("quantity").GetInt32();
+
+                        decimal totalCost = priceEach * quantity;
+                        return totalCost;
+                    }
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+
+            return 0;
+        }
+    }
+}
